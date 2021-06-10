4d8f27591e84aaa9d5a7461f06c40f8538caac46
hama1185 <hamataku0120@gmail.com>
2021-06-10 11:50:40 +0900

    add vision able button

--- Count/Assets/Scripts/ButtonManger.cs
+++ Count/Assets/Scripts/ButtonManger.cs
@@ -5,10 +5,21 @@ using UnityEngine.UI;
 
 public class ButtonManger : MonoBehaviour
 {
+    public GameObject SettingVolume;
     // Start is called before the first frame update
     void Start(){
         
     }
+    public void onClickSettingButton(){
+        if(SettingVolume.activeSelf){
+            SettingVolume.SetActive(false);
+        }
+        else{
+            SettingVolume.SetActive(true);
+        }
+    }
+
+    // クリアはスタート時のタイマーに戻ること
 
     
 }
---
+++ Count/Assets/Scripts/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Michsky.UI.ModernUIPack;
+using Michsky;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    // 時間
+    public int hour = 1;
+    public int min = 0;
+    public int sec = 0;
+
+    public GameObject hourSlider;
+    public GameObject minSlider;
+    public GameObject secSlider;
+
+    // Start is called before the first frame update
+
+    public void onChangeHourVolume(){
+        hour = (int)hourSlider.GetComponent<Slider>().value;
+    }
+
+    public void onChangeMinVolume(){
+        
+    }
+
+    public void onChangeSecVolume(){
+        
+    }
+}
